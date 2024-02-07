19e010375d02a42d77cd5f6cb78bf8cc2079567e
Andrea Diaz Correia <andrea.diaz.correia@gmail.com>
2024-02-07 16:19:07 -0300

    fix: special_num exec

--- code_1.py
+++ code_1.py
@@ -17,6 +17,6 @@ def special_sum(numbers):
 
     return even_sum, odd_product
 
-numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20]
+numbers = [1, 2, 3, 4, 5, 6, 7]
 resultado = special_sum(numbers)
 print("Resultado de la suma especial y el producto:", resultado)
